6ca23e560a6f2fd0f773275d7b62fef49c878696
Jeffrey Tremblay <jeffrey.tremblay26@gmail.com>
2020-09-30 18:26:19 -0400

    Use react-native testing library instead react test renderer

--- app/__tests__/App-test.js
+++ app/__tests__/App-test.js
@@ -1,10 +1,15 @@
-import 'react-native';
+import { render } from '@testing-library/react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+let app;
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App component', () => {
+  beforeEach(() => {
+    app = render(<App />);
+  });
+
+  it('renders correctly', () => {
+    expect(app.toJSON()).toMatchSnapshot();
+  });
 });
