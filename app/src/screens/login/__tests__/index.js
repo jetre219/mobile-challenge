5f88d6fce808cf7766c6b9d730eb6ffd7cde6b8c
Jeffrey Tremblay <jeffrey.tremblay26@gmail.com>
2020-10-01 19:43:07 -0400

    Add login screen

---
+++ app/src/screens/login/__tests__/index.js
@@ -0,0 +1,28 @@
+import { render, fireEvent } from '@testing-library/react-native';
+import React from 'react';
+import { LoginScreen } from '..';
+
+let homeScreen;
+
+const login = jest.fn();
+
+describe('Home screen', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    homeScreen = render(<LoginScreen login={login} />);
+  });
+
+  it('renders correctly', () => {
+    expect(homeScreen.toJSON()).toMatchSnapshot();
+  });
+
+  describe('when pressing login button', () => {
+    beforeEach(() => {
+      fireEvent.press(homeScreen.getByText("Login"))
+    });
+
+    it('calls the login function', () => {
+      expect(login).toHaveBeenCalled();
+    });
+  });
+});
